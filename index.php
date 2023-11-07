cdd83df1c0805cbc90a89b9812a58b9fdb6571cc
Emil <67525639+emil3h@users.noreply.github.com>
2023-11-07 09:17:52 -0800

    added meta tags for SEO

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ include_once 'config.php';
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Quickly create a simple portfolio you can share with a link.">
+  <meta name="keywords" content="portfolio, generator, portfolio generator, free, fast, student portfolio">
   <title>Generator</title>
   <link rel="stylesheet" type="text/css" href="main/index.css">
 </head>
